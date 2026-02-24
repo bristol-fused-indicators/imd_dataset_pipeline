19eee677743d89a4c62f31aabeaf5c243a1c51bc
danielmhi111 <danielmartinhill@gmail.com>
2026-02-24 11:26:27 +0000

    lr init refactor to work with pipeline patterns

--- imd_pipeline/fetch/land_registry.py
+++ imd_pipeline/fetch/land_registry.py
@@ -1,65 +1,93 @@
-from datetime import datetime
-from pathlib import Path
+# from datetime import datetime
+# from pathlib import Path
 
-from loguru import logger
-from project_paths import paths
+# from loguru import logger
+# from project_paths import paths
 
-from imd_pipeline.utils.http import cached_fetch, create_session
+# from imd_pipeline.utils.http import cached_fetch, create_session
 
-BASE_URL = (
-    "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/"
-)
-MONTHLY_UPDATE_URL = f"{BASE_URL}pp-monthly-update-new-version.csv"
-YEARLY_URL_TEMPLATE = f"{BASE_URL}pp-{{year}}.csv"
+# BASE_URL = (
+#     "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/"
+# )
+# MONTHLY_UPDATE_URL = f"{BASE_URL}pp-monthly-update-new-version.csv"
+# YEARLY_URL_TEMPLATE = f"{BASE_URL}pp-{{year}}.csv"
 
-RAW_DIR = paths.data_raw / "land_registry"
+# RAW_DIR = paths.data_raw / "land_registry"
 
-COLUMNS = [
-    "transaction_id",
-    "price",
-    "date_of_transfer",
-    "postcode",
-    "property_type",
-    "old_new",
-    "duration",
-    "paon",
-    "saon",
-    "street",
-    "locality",
-    "town_city",
-    "district",
-    "county",
-    "ppd_category",
-    "record_status",
-]
-
-
-def fetch_yearly(year: int, session, force: bool = False) -> Path:
-    url = YEARLY_URL_TEMPLATE.format(year=year)
-    dest = RAW_DIR / f"pp-{year}.csv"
-    logger.info("fetching yearly land registry data", year=year)
-    return cached_fetch(url, dest, session, force=force)
+# COLUMNS = [
+#     "transaction_id",
+#     "price",
+#     "date_of_transfer",
+#     "postcode",
+#     "property_type",
+#     "old_new",
+#     "duration",
+#     "paon",
+#     "saon",
+#     "street",
+#     "locality",
+#     "town_city",
+#     "district",
+#     "county",
+#     "ppd_category",
+#     "record_status",
+# ]
 
 
-def fetch_monthly_update(session, force: bool = False) -> Path:
-    date_key = datetime.now().strftime("%Y-%m")
-    dest = RAW_DIR / f"pp-monthly-update-{date_key}.csv"
-    logger.info("fetching monthly land registry update", date_key=date_key)
-    return cached_fetch(MONTHLY_UPDATE_URL, dest, session, force=force)
+# def fetch_yearly(year: int, session, force: bool = False) -> Path:
+#     url = YEARLY_URL_TEMPLATE.format(year=year)
+#     dest = RAW_DIR / f"pp-{year}.csv"
+#     logger.info("fetching yearly land registry data", year=year)
+#     return cached_fetch(url, dest, session, force=force)
 
 
-def fetch(force: bool = False, bootstrap_years: list[int] | None = None) -> Path:
+# def fetch_monthly_update(session, force: bool = False) -> Path:
+#     date_key = datetime.now().strftime("%Y-%m")
+#     dest = RAW_DIR / f"pp-monthly-update-{date_key}.csv"
+#     logger.info("fetching monthly land registry update", date_key=date_key)
+#     return cached_fetch(MONTHLY_UPDATE_URL, dest, session, force=force)
+
+
+# def fetch(force: bool = False, bootstrap_years: list[int] | None = None) -> Path:
+#     session = create_session()
+
+#     if bootstrap_years:
+#         for year in bootstrap_years:
+#             fetch_yearly(year, session, force=force)
+
+#     fetch_monthly_update(session, force=force)
+
+#     logger.info("land registry fetch complete", directory=str(RAW_DIR))
+#     return RAW_DIR
+
+from datetime import date
+
+from dateutil.relativedelta import relativedelta
+from icecream import ic
+from requests import Session
+
+from imd_pipeline.utils.http import create_session
+
+
+def fetch_yearly(session: Session, year: int): ...
+
+
+def _required_years(snapshot_date: str, window_months: int) -> list[int]:
+    end = date.fromisoformat(snapshot_date)
+    start = end - relativedelta(months=window_months)
+    return list(range(start.year, end.year + 1))
+
+
+def fetch(snapshot_date: str, window_months: int, force_refresh: bool = False):
     session = create_session()
 
-    if bootstrap_years:
-        for year in bootstrap_years:
-            fetch_yearly(year, session, force=force)
-
-    fetch_monthly_update(session, force=force)
-
-    logger.info("land registry fetch complete", directory=str(RAW_DIR))
-    return RAW_DIR
+    required_years = _required_years(snapshot_date, window_months)
+    ic(required_years)
+    for year in required_years:
+        fetch_yearly(session=session, year=year)
 
 
 if __name__ == "__main__":
-    fetch(bootstrap_years=[2025])
+    snapshot_date = "2025-12-01"
+    window_months = 12
+    fetch(snapshot_date, window_months)
