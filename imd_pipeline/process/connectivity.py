1a278f57e09d66160f1b7fff05c069e9bbe649d7
danielmhi111 <danielmartinhill@gmail.com>
2026-03-03 18:03:00 +0000

    connectivity process docstring

--- imd_pipeline/process/connectivity.py
+++ imd_pipeline/process/connectivity.py
@@ -7,7 +7,16 @@ from imd_pipeline.utils.lsoas import filter_bristol
 
 
 def process() -> pl.LazyFrame:
-    logger.info("processing connectivity data", source=str(paths.data_raw / "connectivity.parquet"))
+    """Loads raw connectivity data, standardises the LSOA code column, and filters to Bristol.
+
+    Returns:
+        LazyFrame of Bristol connectivity metrics with lsoa_code as the key column.
+    """
+
+    logger.info(
+        "processing connectivity data",
+        source=str(paths.data_raw / "connectivity.parquet"),
+    )
     return (
         pl.scan_parquet(paths.data_raw / "connectivity.parquet")
         .select(pl.col("LSOA21CD").alias("lsoa_code"), sls.exclude(pl.col("LSOA21CD")))
