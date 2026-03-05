663b52deb2a74edee7fb7195e0c7db38e58b871a
danielmhi111 <danielmartinhill@gmail.com>
2026-03-05 19:05:15 +0000

    spin up dagster locally

--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 
-from dagster import definitions, load_from_defs_folder
+from dagster import Definitions, definitions, load_from_defs_folder
+
+from .defs.assets import all_assets
 
 
 @definitions
 def defs():
-    return load_from_defs_folder(path_within_project=Path(__file__).parent)
+    defs = Definitions(assets=all_assets)
+    return defs
