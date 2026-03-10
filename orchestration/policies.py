94d779f0a4c40cf4cfda22c5719ff043e3c6ab35
danielmhi111 <danielmartinhill@gmail.com>
2026-03-10 18:07:06 +0000

    adjust freshness policies - lower bound should be time until data becomes stale from cron time (i think)

--- orchestration/policies.py
+++ orchestration/policies.py
@@ -3,13 +3,13 @@ from datetime import timedelta
 from dagster import FreshnessPolicy
 
 quarterly_freshness_policy = FreshnessPolicy.cron(
-    deadline_cron="0 0 1 */3 *", lower_bound_delta=timedelta(minutes=1)
+    deadline_cron="0 0 1 */3 *", lower_bound_delta=timedelta(days=92)
 )
 
 monthly_freshness_policy = FreshnessPolicy.cron(
-    deadline_cron="0 0 1 * *", lower_bound_delta=timedelta(minutes=1)
+    deadline_cron="0 0 1 * *", lower_bound_delta=timedelta(days=31)
 )
 
 annual_freshness_policy = FreshnessPolicy.cron(
-    deadline_cron="0 0 1 1 *", lower_bound_delta=timedelta(minutes=1)
+    deadline_cron="0 0 1 1 *", lower_bound_delta=timedelta(days=365)
 )
