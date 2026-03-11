3b42b6fc39d25e66aace8205ed97100678d6450a
danielmhi111 <danielmartinhill@gmail.com>
2026-03-11 22:00:25 +0000

    add processed assets to run config of lr and crime schedules. rewriting the run config helper to take a list of assets and produce all the configs

--- orchestration/schedules.py
+++ orchestration/schedules.py
@@ -18,10 +18,12 @@ crons = {
 }
 
 
-def _run_config(asset_name: str):
+def _run_config(*asset_names: str):
     def _inner(context):
         snapshot = date.today().replace(day=1).strftime("%Y-%m-%d")
-        return RunConfig(ops={asset_name: TimeframeConfig(snapshot_date=snapshot)})
+        config = TimeframeConfig(snapshot_date=snapshot)
+        ops = {asset_name: config for asset_name in asset_names}
+        return RunConfig(ops=ops)
 
     return _inner
 
@@ -30,35 +32,35 @@ land_registry_schedule = ScheduleDefinition(
     name="land_registry_schedule",
     cron_schedule=crons.get("monthly"),
     job=refresh_lr_job,
-    run_config_fn=_run_config("land_registry_raw_data"),
+    run_config_fn=_run_config("land_registry_raw_data", "land_registry_processed_data"),
 )
 
 crime_schedule = ScheduleDefinition(
     name="crime_schedule",
     cron_schedule=crons.get("monthly"),
     job=refresh_crime_job,
-    run_config_fn=_run_config(asset_name="crime_raw_data"),
+    run_config_fn=_run_config("crime_raw_data", "crime_processed_data"),
 )
 
 universal_credit_schedule = ScheduleDefinition(
     name="universal_credit_schedule",
     cron_schedule=crons.get("monthly"),
     job=refresh_uc_job,
-    run_config_fn=_run_config(asset_name="universal_credit_raw_data"),
+    run_config_fn=_run_config("universal_credit_raw_data"),
 )
 
 connectivity_schedule = ScheduleDefinition(
     name="connectivity_schedule",
     cron_schedule=crons.get("annual"),
     job=refresh_connectivity_job,
-    run_config_fn=_run_config(asset_name="connectivity_raw_data"),
+    run_config_fn=_run_config("connectivity_raw_data"),
 )
 
 open_street_map_schedule = ScheduleDefinition(
     name="open_street_map_schedule",
     cron_schedule=crons.get("quarterly"),
     job=refresh_osm_job,
-    run_config_fn=_run_config(asset_name="open_street_map_raw_data"),
+    run_config_fn=_run_config("open_street_map_raw_data"),
 )
 
 all_schedules = [
