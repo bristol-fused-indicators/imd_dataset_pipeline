ca12ea76862c43c6becde79b69c08902c4f71cd8
DanielSeanBrown <danielseanbrown@gmail.com>
2026-03-24 22:03:19 +0000

    reworked suggested changes on url matching

--- imd_pipeline/fetch/population_lookup.py
+++ imd_pipeline/fetch/population_lookup.py
@@ -7,9 +7,13 @@ from project_paths import paths
 from imd_pipeline.utils.http import create_session
 
 POPULATION_DOWNLOAD_URLS = {
-"2025": "https://opendata.westofengland-ca.gov.uk/api/explore/v2.1/catalog/datasets/population-by-age-band-and-lsoa/exports/csv?lang=en&timezone=Europe%2FLondon&use_labels=true&delimiter=%2C",
-"2022_2023_2024" : "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimatesnationalstatistics/mid2022revisednov2025tomid2024/sapelsoabroadage20222024.xlsx",
-"2019_2020_2021" : "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimatesnationalstatistics/mid2011tomid2022/sapelsoabroadage20112022.xlsx"
+"2025" : "https://opendata.westofengland-ca.gov.uk/api/explore/v2.1/catalog/datasets/population-by-age-band-and-lsoa/exports/csv?lang=en&timezone=Europe%2FLondon&use_labels=true&delimiter=%2C",
+"2022" : "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimatesnationalstatistics/mid2022revisednov2025tomid2024/sapelsoabroadage20222024.xlsx",
+"2023" : "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimatesnationalstatistics/mid2022revisednov2025tomid2024/sapelsoabroadage20222024.xlsx",
+"2024" : "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimatesnationalstatistics/mid2022revisednov2025tomid2024/sapelsoabroadage20222024.xlsx",
+"2019" : "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimatesnationalstatistics/mid2011tomid2022/sapelsoabroadage20112022.xlsx",
+"2020" : "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimatesnationalstatistics/mid2011tomid2022/sapelsoabroadage20112022.xlsx",
+"2021" : "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimatesnationalstatistics/mid2011tomid2022/sapelsoabroadage20112022.xlsx"
 }
 
 def fetch(snapshot_date: str = "2025-12-01", force_refresh: bool = False):
@@ -21,10 +25,11 @@ def fetch(snapshot_date: str = "2025-12-01", force_refresh: bool = False):
         logger.debug("cache hit", path=output_path)
         return
 
-    url = next(
-    (v for k, v in POPULATION_DOWNLOAD_URLS.items() if snapshot_year in k),
-    None
-    )
+    if snapshot_year in POPULATION_DOWNLOAD_URLS.keys():
+        url = POPULATION_DOWNLOAD_URLS[snapshot_year]
+    else:
+        logger.debug("Snapshot date outside of available dates for population data, feature skipped")
+        return
 
     session = create_session()
     logger.info("downloading population_lookup data", url=url)
@@ -54,5 +59,7 @@ if __name__ == "__main__":
     fetch(snapshot_date = "2021-12-01")
     fetch(snapshot_date = "2022-12-01")
     fetch(snapshot_date = "2023-12-01")
+    fetch(snapshot_date = "1990-12-01")
     fetch(snapshot_date = "2024-12-01")
+    fetch(snapshot_date=  "2025-12-01")
     
