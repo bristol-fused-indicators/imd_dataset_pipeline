af99bb1bcf3b41c0112a5c170d50c560f78cd7d9
danielmhi111 <danielmartinhill@gmail.com>
2026-03-29 23:54:50 +0100

    remove imports for code that will break - these functions should be refactored as a fast follow to the multi city pipeline working

--- imd_pipeline/process/__init__.py
+++ imd_pipeline/process/__init__.py
@@ -3,9 +3,9 @@ from imd_pipeline.process import (
     land_registry,
     open_street_map,
     police_uk,
-    universal_credit,
-    geography_lookup,
+    # lsoa_2011_2021_lookup,
+    population_lookup,
+    # geography_lookup,
     postcode_lookup,
-    lsoa_2011_2021_lookup,
-    population_lookup
+    universal_credit,
 )
